e99e4992d0e9d4ebaeeda4b03f82dfba3899f665
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-11 17:20:36 -0500

    commit tree f56faaf586ae240ae0a367c611aa4d6efb852c78

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+const findMatching = (array, string) => {
+    return array.filter(name => name.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(name => name.toLowerCase().startsWith(string.toLowerCase()))
+}
+
+const matchName = function(array, string){ 
+    return array.filter(driver => driver.name === string) 
+}
